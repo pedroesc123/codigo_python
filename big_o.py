e3fb46064eaeac883baa4ea49273025e18776ba8
Pedro Espinoza <pedroesc.9905@gmail.com>
2021-02-21 20:24:00 -0500

    Agregado el Big O

---
+++ big_o.py
@@ -0,0 +1,42 @@
+import time
+import math
+
+def constante(n):
+    return 1
+
+def logaritmico(n):
+    return math.log10(n)
+
+def lineal(n):
+    return n
+
+def lin_log(n):
+    return n * math.log10(n)
+
+def cuadratica(n):
+    return n ** 2
+
+def exponencial(n):
+    return 2 ** n
+
+if __name__ == '__main__':
+    n =[1, 10, 100, 1000, 10000]
+
+    # for i in n:
+        # comienzo = time.time()
+        # print(constante(i))
+        # final = time.time()
+        # print(f'El tiempo usado es: {final - comienzo}')
+
+        # comienzo = time.time()
+        # print(logaritmico(i))
+        # final = time.time()
+        # print(f'El tiempo usado es: {final - comienzo}')
+
+    comienzo = time.time()
+    print(exponencial(10000))
+    final = time.time()
+    print(final - comienzo)
+
+    print ('Ahora pasaremos al siguiente valor de n \n')
+
