f711b17428ce41f253c9828bee1281e12f8ee662
Pedro Espinoza <pedroesc.9905@gmail.com>
2021-02-23 13:14:45 -0500

    Agregando el archivo de búsqueda lineal

---
+++ busqueda_lineal.py
@@ -0,0 +1,23 @@
+import random
+
+def busqueda_lineal(lista, objetivo):
+    match = False
+
+    for elemento in lista: # O(n)
+        if elemento == objetivo:
+            match = True
+            break
+    
+    return match
+
+
+if __name__ == '__main__':
+    tamano_lista = int(input("De que tamaño será la lista?: "))
+    objetivo = int(input("Qué número quieres encontrar?: "))
+
+    lista = [random.randint(0, 100) for i in range(tamano_lista)]
+    #Se genera una lista del tamaño del range, llena de numeros aleatorios
+
+    encontrado = busqueda_lineal(lista, objetivo)
+    print(lista)
+    print(f'El elemento {objetivo} {"esta" if encontrado else "no esta"} en la lista')
